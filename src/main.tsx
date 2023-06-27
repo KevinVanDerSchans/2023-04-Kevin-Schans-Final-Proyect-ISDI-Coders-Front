d48fbb5437d2b616b3346a18fd4c797cdb82687c
Kevin Schans <kevinvdsd@hotmail.com>
2023-06-28 00:21:26 +0200

    Complete main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./core/store/store";
+import { App } from "./features/components/app/app";
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
   <React.StrictMode>
-
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App></App>
+    </Provider>
+  </React.StrictMode>
+);

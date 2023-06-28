4501840cd123b302c2b68273a11723acc0b8112f
Kevin Schans <kevinvdsd@hotmail.com>
2023-06-28 18:01:11 +0200

    Create footer.tsx

---
+++ src/features/components/footer/footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return(
+    <footer>
+      <div>
+        <span>Alex & Melanie © 2023</span>
+        <address>Kevin Schans | ISDI Coders</address>
+      </div>
+    </footer>
+  )
+}

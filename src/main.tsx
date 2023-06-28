cf12b72b17c70812bc3a60936a72debb23f84222
Kevin Schans <kevinvdsd@hotmail.com>
2023-06-28 21:12:30 +0200

    Adjust footer styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./core/store/store";
 import { App } from "./features/components/app/app";
-import './main.css'
 import { BrowserRouter as Router } from 'react-router-dom';
+import './main.css'
 
 ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
   <React.StrictMode>

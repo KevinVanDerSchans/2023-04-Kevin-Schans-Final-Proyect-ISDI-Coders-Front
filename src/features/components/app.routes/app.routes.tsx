686dcaad89db39deeb500f8ba7245c2fb7198bf2
Kevin Schans <kevinvdsd@hotmail.com>
2023-06-28 17:44:56 +0200

    Add home route

--- src/features/components/app.routes/app.routes.tsx
+++ src/features/components/app.routes/app.routes.tsx
@@ -1,6 +1,8 @@
 import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+const Home = lazy(() => import('../home/home'));
+
 export function AppRoutes() {
   return (
     <Suspense>

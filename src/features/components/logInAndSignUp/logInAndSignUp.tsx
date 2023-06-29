957d55d2cc43d072ec8b974196343eeac3104218
Kevin Schans <kevinvdsd@hotmail.com>
2023-06-29 21:22:36 +0200

    Add video to lfs git and add styles

--- src/features/components/logInAndSignUp/logInAndSignUp.tsx
+++ src/features/components/logInAndSignUp/logInAndSignUp.tsx
@@ -1,10 +1,12 @@
 import Login from "../login/login"
 import Register from "../register/register"
 import style from "../logInAndSignUp/logInAndSignUp.module.css"
+import { LogInAndSignUpVideo } from "./logInAndSignUpVideo/logInAndSignUpVideo";
 
 export default function LogInAndSignUp() {
   return (
     <div className={style.body}>
+      <LogInAndSignUpVideo></LogInAndSignUpVideo>
       <Register></Register>
       <Login></Login>
     </div>

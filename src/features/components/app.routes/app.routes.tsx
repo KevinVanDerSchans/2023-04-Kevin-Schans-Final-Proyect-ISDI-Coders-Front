b5384d20593e56a58eeaf693517c7db6b8e7e369
Kevin Schans <kevinvdsd@hotmail.com>
2023-06-29 09:18:39 +0200

    Add MyCourses Route in app.routes.tsx

--- src/features/components/app.routes/app.routes.tsx
+++ src/features/components/app.routes/app.routes.tsx
@@ -2,12 +2,14 @@ import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 const Home = lazy(() => import('../home/home'));
+const MyCourses = lazy(() => import())
 
 export function AppRoutes() {
   return (
     <Suspense>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/myCourses' element={<MyCourses></MyCourses>}></Route>
       </Routes>
     </Suspense>
   );

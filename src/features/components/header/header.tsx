f6946567925a928c982773b7d8c00dd59dad740a
Kevin Schans <kevinvdsd@hotmail.com>
2023-06-30 16:12:23 +0200

    Add header.test.tsx

--- src/features/components/header/header.tsx
+++ src/features/components/header/header.tsx
@@ -1,8 +1,8 @@
 import { NavControls } from '../navControls/nav.controls';
 import { Nav } from '../nav/nav';
 import { Link } from 'react-router-dom';
-import style from './header.module.css';
 import { NavOptions } from '../../types/nav.options';
+import style from './header.module.css';
 
 export type Propstype = {
   options: NavOptions[];
@@ -10,14 +10,14 @@ export type Propstype = {
 
 export function Header({ options }: Propstype) {
   return (
-      <header className={style.header} id="header">
-        <div className={style.logoContainer}>
-          <Link to='/'>
-            <img className={style.logo} src='/assets/logo.png' alt="Alex & Melanie's company logo"/>
-          </Link>
-        </div>
-        <NavControls></NavControls>
-        <Nav options={options}></Nav>
-      </header>
+    <header className={style.header}>
+      <div className={style.logoContainer}>
+        <Link to='/'>
+          <img className={style.logo} src='/assets/logo.png' alt="Alex & Melanie's company logo"/>
+        </Link>
+      </div>
+      <NavControls></NavControls>
+      <Nav options={options}></Nav>
+    </header>
   );
 }

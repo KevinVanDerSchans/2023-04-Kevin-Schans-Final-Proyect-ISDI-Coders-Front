18c643aa9a516d2d6fe65405879cd4d914e47dd9
Kevin Schans <kevinvdsd@hotmail.com>
2023-07-01 15:48:15 +0200

    Add Header and Footer to contact.tsx

--- src/features/components/contact/contact.tsx
+++ src/features/components/contact/contact.tsx
@@ -1,6 +1,19 @@
+import { NavOptions } from "../../types/nav.options";
+import { Footer } from "../footer/footer";
+import { Header } from "../header/header";
+
 export default function Contact() {
+
+  const options: NavOptions[] = [
+    { url: '/', label: 'HOME' },
+    { url: '/myCourses', label: 'MY COURSES' },
+    { url: '/contact', label: 'CONTACT '}
+  ];
+
   return(
     <>
+    <Header options={options}></Header>
+    <Footer></Footer>
     </>
-  )
+  );
 }

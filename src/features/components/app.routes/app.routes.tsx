af755b996ae51fdb561569137fff6ebc2b6f2ec1
Kevin Schans <kevinvdsd@hotmail.com>
2023-07-03 09:40:14 +0200

    Add my.courses.test.tsx

--- src/features/components/app.routes/app.routes.tsx
+++ src/features/components/app.routes/app.routes.tsx
@@ -2,7 +2,7 @@ import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 const Home = lazy(() => import('../home/home'));
-const MyCourses = lazy(() => import('../my.courses/myCourses'));
+const MyCourses = lazy(() => import('../my.courses/my.courses'));
 const Contact = lazy(() => import('../contact/contact'));
 
 const LogInAndSignUp = lazy(() => import('../logInAndSignUp/log.in.and.sign.up'));

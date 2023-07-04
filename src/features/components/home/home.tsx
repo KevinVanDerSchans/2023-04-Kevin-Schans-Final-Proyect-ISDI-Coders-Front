e2d2e7fa6c17000c13a560ea257ac5821314d9fe
Kevin Schans <kevinvdsd@hotmail.com>
2023-07-04 18:57:30 +0200

    Comment role in home.tsx for now

--- src/features/components/home/home.tsx
+++ src/features/components/home/home.tsx
@@ -2,8 +2,8 @@ import { Header } from '../header/header';
 import { NavOptions } from '../../types/nav.options';
 import { Footer } from '../footer/footer';
 import { Hero } from './hero/hero';
-import { store } from '../../../core/store/store';
-import style from "../home/home.module.css"
+// import { store } from '../../../core/store/store';
+// import style from "../home/home.module.css"
 
 export default function Home() {
   const options: NavOptions[] = [
@@ -11,13 +11,14 @@ export default function Home() {
     { url: '/myCourses', label: 'MY COURSES' },
     { url: '/contact', label: 'CONTACT' }
   ];
-
+  /* TEMP
   const admin = store.getState().users.role;
   console.log(admin);
 
   if (admin === 'admin') {
     <span className={style.hola}>HOLA</span>
   }
+  */
 
   return (
     <>

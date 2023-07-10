bfb6bad5e70b728dcb9d5c87db6599fe7c13a4e5
Kevin Schans <kevinvdsd@hotmail.com>
2023-07-10 10:43:22 +0200

    Fix danceCourse.card.test.tsx

--- src/features/components/my.courses/list/danceCourse.card/danceCourse.card.test.tsx
+++ src/features/components/my.courses/list/danceCourse.card/danceCourse.card.test.tsx
@@ -2,38 +2,40 @@ import { DanceCourseCard } from "./danceCourse.card";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter as Router } from "react-router-dom";
 import "@testing-library/jest-dom";
-
+import { store } from "../../../../../core/store/store";
+import { Provider } from "react-redux";
 
 describe("Given the DanceCourseCard component", () => {
   describe("When it is intantiated", () => {
 
     render(
-      <Router>
-        <DanceCourseCard item={{
-          id: '3',
-          courseName: 'line salsa',
-          level: 'Intermediate',
-          priceCourse: '19,99',
-          totalClasses: 10,
-          initialDescription: 'abc',
-          largeDescription: "abcdef",
-          image: {
-            urlOriginal: "",
-            url: "https://example.image.jpg",
-            mimetype: "",
-            size: 0,
-          },
-          video: {
-            urlOriginal: "",
-            url: "https://example.video.jpg",
-            mimetype: "",
-            size: 0,
-          },
-        }}
+      <Provider store={store}>
+        <Router>
+          <DanceCourseCard item={{
+            id: '3',
+            courseName: 'line salsa',
+            level: 'Intermediate',
+            priceCourse: '19,99',
+            totalClasses: 10,
+            initialDescription: 'abc',
+            largeDescription: "abcdef",
+            image: {
+              urlOriginal: "",
+              url: "https://example.image.jpg",
+              mimetype: "",
+              size: 0,
+            },
+            video: {
+              urlOriginal: "",
+              url: "https://example.video.jpg",
+              mimetype: "",
+              size: 0,
+            },
+          }}
           ></DanceCourseCard>
-      </Router>
+        </Router>
+      </Provider>
     );
-
 
     test("Then it should be in the document", () => {
       const element = screen.getByRole("listitem");

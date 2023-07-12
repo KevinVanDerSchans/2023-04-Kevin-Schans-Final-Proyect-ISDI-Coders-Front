4ea482fb0551512044cd7dfc5b236d3c14a48c03
Kevin Schans <kevinvdsd@hotmail.com>
2023-07-12 13:05:35 +0200

    Change video to webm and add music functionality

--- src/features/components/logInAndSignUp/log.in.and.sign.up.video/log.in.and.sign.up.video.tsx
+++ src/features/components/logInAndSignUp/log.in.and.sign.up.video/log.in.and.sign.up.video.tsx
@@ -1,12 +1,49 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import style from "../log.in.and.sign.up.video/log.in.and.sign.up.video.module.css"
+import { faMusic, faVolumeXmark } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export function LogInAndSignUpVideo() {
+
+  const [muted, setMuted] = useState(true);
+
+  const handleMusicControlClick = () => {
+    setMuted((prevMuted) => !prevMuted);
+  };
+
+  const buttonClasses = [muted ? "" : "controlVolume", style.customButton];
+
   return(
     <section>
+
       <div className={style.logInAndSignUpVideoContainer}>
           <div className={style.logInAndSignUpVideoWrapper}>
-          <video className={style.logInAndSignUpVideo} src="/assets/bachataFusion.mp4" role="video" muted autoPlay loop></video>
+            <video muted={muted} className={style.logInAndSignUpVideo} src="/assets/logInAndSignUp.webm" role="video" autoPlay loop></video>
           </div>
+      </div>
+
+      <div className={style.controlMusicButtonContainer}>
+        <button
+          id="musicControl"
+          className={buttonClasses.join(' ')}
+          onClick={handleMusicControlClick}>
+          {muted ? (
+        <>
+        <FontAwesomeIcon className={style.musicOnIcon}
+          aria-label="Icon Music to put the music ON"
+          icon={faMusic}
+          beat
+          style={{ color: "#e5e5e6" }}
+        />
+        </>
+        ) : (
+        <>
+          <FontAwesomeIcon className={style.musicOffIcon}
+            icon={faVolumeXmark}
+            style={{color: "#efeff1",}} />
+        </>
+        )}
+        </button>
       </div>
     </section>
   );

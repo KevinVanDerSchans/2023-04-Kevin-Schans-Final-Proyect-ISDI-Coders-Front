03b3d98f76addd2792838bd1ffbfb8dafd925bff
Kevin Schans <kevinvdsd@hotmail.com>
2023-08-04 13:58:12 +0200

    Change render url in hooks

--- src/features/hooks/use.users.ts
+++ src/features/hooks/use.users.ts
@@ -9,7 +9,7 @@ export function useUsers() {
   const { users, currentUser, token, loginError } = useSelector((state: RootState) => state.users);
   const dispatch: AppDispatch = useDispatch();
 
-  const url = "http://localhost:4545/";
+  const url = "https://kevin-schans-alexandmelanie.onrender.com/user";
 
   const repo: UserRepository = useMemo(() => new UserRepository(url), []);
 
